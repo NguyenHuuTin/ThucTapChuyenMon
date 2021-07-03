32680a2382ed98e445aa1bd48f78f9804c70085c
NguyenHuuTin <5951071106@st.utc2.edu.vn>
2021-07-03 22:11:01 +0700

    Hoan Thanh

--- PageAdmin/admin/Order/index.php
+++ PageAdmin/admin/Order/index.php
@@ -31,6 +31,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
+	 <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+	  <!-- Ionicons -->
+	  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 	<!-- jQuery library -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
@@ -55,6 +58,7 @@
 	    	</div>
 	        <ul class="nav flex-column nav-pills" role="tablist">
 	        		<li class="nav-item">
+	        			<i class="nav-icon fas fa-tachometer-alt"></i>
 				    <a class="nav-link active" href="">Đơn Hàng Mới</a>
 				  </li>
 				  <li class="nav-item">
